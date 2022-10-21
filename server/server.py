dac8193a6f527551ad0e043fa283ec7c57bc074a
jgbf <kemuaat@gmail.com>
2022-10-21 22:33:45 +0200

    add render template

--- server/server.py
+++ server/server.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('../ui')
+app = Flask(__name__, template_folder=template_dir)
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5001)
